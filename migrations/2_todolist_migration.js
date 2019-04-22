14c1f80e2fe86e50a28225ead959752d943dffd0
yosuke.aramaki <gnx.vw903@gmail.com>
2019-04-22 13:50:20 +0900

    commit

--- migrations/2_todolist_migration.js
+++
@@ -1,7 +0,0 @@
-var TodoList = artifacts.require("./TodoList.sol");
-
-module.exports = function(deployer) {
-  deployer.deploy(TodoList).then(instance => {
-    console.log('ABI:',JSON.stringify(instance.abi))
-  })  
-};
